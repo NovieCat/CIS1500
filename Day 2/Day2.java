71e3e58091c9479d78399a38d7e7923b81b0382a
Grace Yang <gyang317@gmail.com>
2019-12-02 17:24:54 -0500

    class exercises

---
+++ Day 2/Day2.java
@@ -0,0 +1,23 @@
+import javax.swing.JOptionPane;
+
+/**
+   whats this
+   @author grace yang
+   @version sep 6 2016
+   cis 1500
+*/
+
+public class Day2
+{
+   
+   //variables for data
+   //instructions for manipulating data
+   
+   public static void main(String [] args)
+   {
+      System.out.println("grace\n");
+      System.out.print("i love java");
+      
+      JOptionPane.showMessageDialog(null, "hoi");
+   }
+}
